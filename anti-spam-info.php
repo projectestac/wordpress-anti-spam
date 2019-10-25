88199f19e24d4ab53fc861b35ac9ffd0cfa2e347
Ignacio Abejaro <ignacio.abejaro@ticxcat.cat>
2019-10-25 09:40:31 +0200

    Version 5.5

--- anti-spam-info.php
+++ anti-spam-info.php
@@ -1,6 +1,8 @@
 <?php
 
-defined('ABSPATH') OR exit; // prevent full path disclosure
+if ( ! defined( 'ABSPATH' ) ) { // Avoid direct calls to this file and prevent full path disclosure
+	exit;
+}
 
 function antispam_admin_notice() {
 	global $pagenow;
@@ -8,16 +10,16 @@ function antispam_admin_notice() {
 		$user_id = get_current_user_id();
 		$antispam_info_visibility = get_user_meta($user_id, 'antispam_info_visibility', true);
 		if ($antispam_info_visibility == 1 OR $antispam_info_visibility == ''):
+			$blocked_total = 0; // show 0 by default
 			$antispam_stats = get_option('antispam_stats', array());
-			$blocked_total = $antispam_stats['blocked_total'];
-			if(empty($blocked_total)){
-				$blocked_total = 0;
+			if (isset($antispam_stats['blocked_total'])) {
+				$blocked_total = $antispam_stats['blocked_total'];
 			}
 			?>
 			<div class="update-nag antispam-panel-info">
 				<p style="margin: 0;">
 					<?php echo $blocked_total; ?> spam comments were blocked by <a href="http://wordpress.org/plugins/anti-spam/">Anti-spam</a> plugin so far.
-					<a href="http://codecanyon.net/item/antispam-pro/6491169?ref=webvitaly" title="Anti-spam Pro">Upgrade to Pro</a> for more advanced protection.
+					<a href="http://codecanyon.net/item/antispam-pro/6491169?ref=webvitalii" title="Anti-spam Pro">Upgrade to Pro</a> for more advanced protection.
 				</p>
 			</div>
 			<?php
